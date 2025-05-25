1fab094fe10b830f6d7e892b110a92a50195a1bf
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-05-25 20:23:06 +0530

    hi

--- Print_Dimond_Shape.java
+++ Print_Dimond_Shape.java
@@ -1,4 +1,4 @@
-public class Inverted_Pyramid {
+public class Print_Dimond_Shape {
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
             for(int j=5;j>=i;j--){
@@ -13,6 +13,21 @@ public class Inverted_Pyramid {
         
         System.out.println();
         }
-    }
+         for(int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5;k>=i;k--){
+                System.out.print("*");
+            }
+            for(int p=5;p>i;p--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     
-}
+     }
+ }
+    
+
+
