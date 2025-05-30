95d75d866e2b460579f0d9d7f04f19a5981b8fc2
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-05-30 23:04:36 +0530

    how are you

---
+++ Transpose_OFMatrix.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Transpose_OFMatrix {
+
+  
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter no. of rows and colomn  ");
+        int r =sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Enter your data Matrix ");
+        int[][] transpose =new int[r][c];
+         for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            transpose[i][j]=sc.nextInt();
+        }
+      }
+      // for output
+      System.out.println("Transpose of  given data is");
+        for(int j=0;j<c;j++){
+        for(int i=0;i<r;i++){
+            System.out.print(transpose[i][j] + "");
+        }
+        System.out.println();
+      }
+    }
+    
+}
