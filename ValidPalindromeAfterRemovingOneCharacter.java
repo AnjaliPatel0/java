6f6b1195ab874448415ce6037f1aa49933614b86
AnjaliPatel0 <anjalipatela78@gmail.com>
2025-06-28 10:38:29 +0530

    hi git

---
+++ ValidPalindromeAfterRemovingOneCharacter.java
@@ -0,0 +1,32 @@
+public class ValidPalindromeAfterRemovingOneCharacter {
+    public static void main(String[] args) {
+        String s = "madame";
+        boolean result = validPalindrome(s);
+        System.out.println(result);
+    }
+    
+    public static boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either the left or the right character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; // The string is already a palindrome
+    }
+
+    public static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
